eff7e8b1a9047205bc053f66592258b95a775c2e
ErinSam <erinsam.joe@gmail.com>
2020-11-12 23:57:56 +0530

    Added the Harten-Hyman Entropy fix. Still need to add the approximation for the star region. This shall require re-writing some of the functions within the Approximate Reimann Solver

--- The Reimann Solver of Roe/roe_reimann_solver.py
+++ The Reimann Solver of Roe/roe_reimann_solver.py
@@ -14,11 +14,13 @@ import math as m
 
 
 
-def roe_reimannS(U_L, U_R):
+def roe_pike_reimann_solver(U_L, U_R):
     """ ROE'S REIMANN SOLVER 
 
         This function uses the reimann solver of roe to solve Reimann Problems for x-split 
         3D Euler Equations. 
+        This version of Roe's Reimann solver was introduced by Roe & Pike. This is does not 
+        involve the entropy fix. 
 
         Args:
             U_L: ndarray(5,); vector of L conserved variable for the x-split 3D Euler equations
@@ -137,3 +139,147 @@ def roe_reimannS(U_L, U_R):
     
     # Return statement 
     return F_0
+
+
+
+def entropy_fix_roe_pike_reimann_solver(U_L, U_R, head_speed, tail_speed, rho_star, direction):
+    """ ENTROPY FIX 
+
+        This function implements the Harten-Hyman Entropy Fix that is required to solve the 
+        RP within a rarefaction
+ 
+        Args:
+            U_L: ndarray(5,); vector of L conserved variable for the x-split 3D Euler equations
+            U_R: ndarray(5,); vector of R conserved variable for the x-split 3D Euler equations
+            head_speed: float; speed of the head as calculated from the chosen form of star
+                                region approximation
+            tail_speed: float; speed of the head as calculated from the chosen form of star
+                                region approximation 
+            rho_star: float; 
+            direction: string; 
+
+        Returns: 
+            intercell_flux: ndarray(5,); vector of intercell numerical flux that is calculated 
+                                            by the Roe Reimann Solver
+    """
+
+    # Obtaining the flow-field variables from the conserved variabl vectors
+    rho_L, u_L, v_L, w_L, E_L = U_L[0], U_L[1]/U_L[0], U_L[2]/U_L[0], U_L[3]/U_L[0], U_L[4] 
+    p_L = (c_ratio-1) * ( E_L - 0.5 * rho_L * (pow(u_L,2) + pow(v_L,2) + pow(w_L,2) ) )
+    H_L = (E_L + p_L) / rho_L
+
+    rho_R, u_R, v_R, w_R, E_R = U_R[0], U_R[1]/U_R[0], U_R[2]/U_R[0], U_R[3]/U_R[0], U_R[4] 
+    p_R = (c_ratio-1) * ( E_R - 0.5 * rho_R * (pow(u_R,2) + pow(v_R,2) + pow(w_R,2) ) )
+    H_R = (E_R + p_R) / rho_R
+
+
+    # Calculation of the Roe - Averaged Variables
+    rho_avg = m.sqrt(rho_L * rho_R)
+    u_avg = ( m.sqrt(rho_L)*u_L + m.sqrt(rho_R)*u_R ) / ( m.sqrt(rho_L) + m.sqrt(rho_R) )
+    v_avg = ( m.sqrt(rho_L)*v_L + m.sqrt(rho_R)*v_R ) / ( m.sqrt(rho_L) + m.sqrt(rho_R) )
+    w_avg = ( m.sqrt(rho_L)*w_L + m.sqrt(rho_R)*w_R ) / ( m.sqrt(rho_L) + m.sqrt(rho_R) )
+    H_avg = ( m.sqrt(rho_L)*H_L + m.sqrt(rho_R)*H_R ) / ( m.sqrt(rho_L) + m.sqrt(rho_R) )
+    a_avg = m.sqrt( (c_ratio-1) * (H_avg - 0.5 * (pow(u_avg,2) + pow(v_avg,2) + pow(w_avg,2)) 
+
+
+    # Calculating the required averaged right eigenvector and averaged wavestrength
+    du, dv, dw, dp, drho = u_R - u_L, v_R - v_L, w_R - w_L, p_R - p_L, rho_R - rho_L
+    if ( direction == "left" ):
+        K = np.array([1,
+                        u_avg - a_avg,
+                        v_avg,
+                        w_avg,
+                        H_avg - u_avg*a_avg])
+        alpha = 1/(2*pow(a_avg,2)) * (dp - rho_avg*a_avg*du)
+    
+    else:
+        K = np.array([1,
+                        u_avg + a_avg,
+                        v_avg,
+                        w_avg,
+                        H_avg + u_avg*a_avg])
+        alpha = 1/(2*pow(a_avg,2)) * (dp + rho_avg*a_avg*du) 
+
+
+    # Calulation of the Roe-Averaged wave speed between the head and tail of the shock wave 
+    if ( direction == "left" ):
+        wave_avg = ( m.sqrt(rho_L)*tail_speed + m.sqrt(rho_star)*head_speed ) \
+                    / ( m.sqrt(rho_L) + m.sqrt(rho_star) ) 
+    else:
+        wave_avg = ( m.sqrt(rho_star)*tail_speed + m.sqrt(rho_R)*head_speed ) \
+                    / ( m.sqrt(rho_star) + m.sqrt(rho_R) ) 
+
+
+    # Calculation of the new wave speed
+    if ( direction == "left" ):
+        new_wave_speed = tail_speed * ( head_speed - wave_avg ) / ( head_speed - tail_speed )
+    else:
+        new_wave_speed = head_speed * ( wave_avg - tail_speed ) / ( head_speed - tail_speed )
+
+
+    # Computing the intercell numerical flux 
+    # Left Flux, F(U_L)
+    F_L = np.array([rho_L * u_L,
+                    rho_L * pow(u_L,2) + p_L,
+                    rho_L * u_L * v_L,
+                    rho_L * u_L * w_L,
+                    u_L * (E_L + p_L)])
+
+    # Right Flux, F(U_R)
+    F_R = np.array([rho_R * u_R,
+                    rho_R * pow(u_R,2) + p_R,
+                    rho_R * u_R * v_R,
+                    rho_R * u_R * w_R,
+                    u_R * (E_R + p_R)])
+    
+
+    # Obtaining the intercell numerical flux 
+    if ( direction == "left" ):
+        intercell_flux = F_L + new_wave_speed * alpha * K
+    else:
+        intercell_flux = F_R - new_wave_speed * alpha * K 
+
+
+    # Return statement
+    return intercell_flux
+
+
+
+
+
+
+
+
+def roe_pike_reimann_solver_main(U_L, U_R):
+    """ ROE PIKE REIMANN SOLVER 
+
+        This funtion implements the Roe-Pike method to solve a RP given the L and R region 
+        properties. It includes the entropy fix that is used to check whether the solution 
+        for the intercell numerical flux is present within a rarefaction wave. 
+
+        Args:
+            U_L: ndarray(5,); vector of L conserved variable for the x-split 3D Euler equations
+            U_R: ndarray(5,); vector of R conserved variable for the x-split 3D Euler equations
+
+        Returns: 
+            intercell_flux: ndarray(5,); vector of intercell numerical flux that is calculated 
+                                            by the Roe Reimann Solver
+    """
+
+    # Heat Capacity Ratio 
+    c_ratio = 1.4
+
+    # Obtaining approximation for the star region with ANRS
+    # TODO
+
+    # Checking whether entropy fix should be used or not 
+    # TODO
+
+    # Calling the right functions 
+    # TODO
+
+    # Return statement
+    # TODO
+
+
+ 
